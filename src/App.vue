f2d435e60c8757de9bd40e76246e80f9a562ad95
Kanvi <kanvisavani123@gmail.com>
2021-10-02 21:57:24 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div v-show="!seen" v-for="post in posts" v-bind:key="post.id">
+    {{ maxVal.push(post.Random)}}
+      </div>
+    <b-card-group>
+    <b-card class="widthOfCard mb-3" v-for="post in posts" v-bind:key="post.id">
+      <b-card-header :class="[Math.max.apply(Math, maxVal) == post.Random ? 'text-danger' : 'text-primary']" >{{ post.user_name }}<b-card-title>{{ post.Random }}</b-card-title></b-card-header>
+      <b-card-body>
+        <b-card-img class="imageSize" :src="require(`@/assets/profileImg/${post.profile_pic}`)" />
+      </b-card-body>
+    </b-card>
+    </b-card-group>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+<style scoped>
+.widthOfCard {
+  max-width: 18rem;
 }
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.imageSize {
+  height: 200px;
+  width:200px;
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [],
+      maxVal: [],
+      seen: true
+    };
+  },
+
+  methods: {
+    async getData() {
+      try {
+        const response = await this.$http.get(
+            "http://localhost:4449/getUsers"
+        );
+        this.posts = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+
+  created() {
+    window.setInterval(() => {
+      this.maxVal=[];
+      this.getData();
+    }, 1000)
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue } from 'bootstrap-vue/dist/bootstrap-vue.esm'
+import axios from 'axios';
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+
+Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false
 
